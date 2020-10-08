1cb1753d882a045466e243e8cbb0be08c23a38a2
changdae20 <changdae20@naver.com>
2020-10-08 22:24:19 +0900

    modified pid.cpp

--- project1/include/project1/pid.cpp
+++ project1/include/project1/pid.cpp
@@ -7,7 +7,12 @@ PID::PID(){
      * find appropriate value of the coefficients for PID control, and initialize them.
      *
     */
-
+    Kp = 1;
+    Ki = 1;
+    Kd = 1;
+    error = 0;
+    error_sum = 0;
+    error_diff = 0;
 }
 
 float PID::get_control(point car_pose, point goal_pose){
@@ -19,6 +24,11 @@ float PID::get_control(point car_pose, point goal_pose){
      * implement pid algorithm
      *
     */
+    error_diff = goal_pose.th - car_pose.th - error
+    error = goal_pose.th - car_pose.th;
+    error_sum = error_sum + error;
+
+    ctrl = Kp * error + Ki * 0.1 * error_sum + (Kd/0.1) * error_diff;
 
     return ctrl;
 }

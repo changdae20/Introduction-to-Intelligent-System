b750a7de1d8b5e56a94323627689392b3776a402
KyungJune <kjune1999@naver.com>
2020-12-09 03:26:57 +0900

    final project

---
+++ final_project/src/main.cpp
@@ -0,0 +1,191 @@
+//state definition
+#define INIT 0
+#define PATH_PLANNING 1
+#define RUNNING 2
+#define FINISH -1
+
+#include <unistd.h>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Time.h>
+#include <ackermann_msgs/AckermannDriveStamped.h>
+#include <project2/rrtTree.h>
+#include <tf/transform_datatypes.h>
+#include <project2/pid.h>
+#include <math.h>
+#include <pwd.h>
+
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+
+//map spec
+cv::Mat map;
+double res;
+int map_y_range;
+int map_x_range;
+double map_origin_x;
+double map_origin_y;
+double world_x_min;
+double world_x_max;
+double world_y_min;
+double world_y_max;
+
+//parameters we should adjust : K, margin, MaxStep
+int margin = 6;
+int K = 500;
+double MaxStep = 2;
+int waypoint_margin = 24;
+
+//way points
+std::vector<point> waypoints;
+
+//path
+std::vector<traj> path_RRT;
+
+//robot
+point robot_pose;
+ackermann_msgs::AckermannDriveStamped cmd;
+
+//FSM state
+int state;
+
+//function definition
+void setcmdvel(double v, double w);
+void callback_state(geometry_msgs::PoseWithCovarianceStampedConstPtr msgs);
+void set_waypoints();
+void generate_path_RRT();
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "slam_main");
+    ros::NodeHandle n;
+
+    // Initialize topics
+    ros::Publisher cmd_vel_pub = n.advertise<ackermann_msgs::AckermannDriveStamped>("/vesc/high_level/ackermann_cmd_mux/input/nav_0",1);
+    
+    ros::Subscriber gazebo_pose_sub = n.subscribe("/amcl_pose", 100, callback_state);
+
+    printf("Initialize topics\n");
+    
+    // FSM
+    state = INIT;
+    bool running = true;
+    ros::Rate control_rate(60);
+
+    while(running){
+        switch (state) {
+        case INIT: {
+            // Load Map
+            char* user = getpwuid(getuid())->pw_name;
+            cv::Mat map_org = cv::imread((std::string("/home/") + std::string(user) +
+                              std::string("/catkin_ws/src/final_project/src/final.pgm")).c_str(), CV_LOAD_IMAGE_GRAYSCALE);
+
+            cv::transpose(map_org,map);
+            cv::flip(map,map,1);
+
+            map_y_range = map.cols;
+            map_x_range = map.rows;
+            map_origin_x = map_x_range/2.0 - 0.5;
+            map_origin_y = map_y_range/2.0 - 0.5;
+            world_x_min = -4.7;
+            world_x_max = 4.7;
+            world_y_min = -10.2;
+            world_y_max = 10.2;
+            res = 0.05;
+            printf("Load map\n");
+
+             if(! map.data )                              // Check for invalid input
+            {
+                printf("Could not open or find the image\n");
+                return -1;
+            }
+            state = PATH_PLANNING;
+        } break;
+
+        case PATH_PLANNING:
+            
+            // Set Way Points
+            set_waypoints();
+            printf("Set way points\n");
+
+            // RRT
+            generate_path_RRT();
+            printf("Generate RRT\n");
+
+            ros::spinOnce();
+            ros::Rate(0.33).sleep();
+
+            printf("Initialize ROBOT\n");
+            state = RUNNING;
+
+        case RUNNING: {
+            //TODO 3
+        } break;
+
+        case FINISH: {
+            setcmdvel(0,0);
+            cmd_vel_pub.publish(cmd);
+            running = false;
+            ros::spinOnce();
+            control_rate.sleep();
+        } break;
+        default: {
+        } break;
+        }
+    }
+    return 0;
+}
+
+void setcmdvel(double vel, double deg){
+    cmd.drive.speed = vel;
+    cmd.drive.steering_angle = deg;
+}
+
+void callback_state(geometry_msgs::PoseWithCovarianceStampedConstPtr msgs){
+    robot_pose.x = msgs->pose.pose.position.x;
+    robot_pose.y = msgs->pose.pose.position.y;
+    robot_pose.th = tf::getYaw(msgs->pose.pose.orientation);
+    //printf("x,y : %f,%f \n",robot_pose.x,robot_pose.y);
+}
+
+void set_waypoints()
+{
+    point waypoint_candid[7];
+
+    // Starting point. (Fixed)
+    waypoint_candid[0].x = -3.5;
+    waypoint_candid[0].y = 8.5;
+
+
+    //TODO 2
+    // Set your own waypoints.
+    // The car should turn around the outer track once, and come back to the starting point.
+    // This is an example.
+    waypoint_candid[1].x = 2.2;
+    waypoint_candid[1].y = 8.5;
+    waypoint_candid[2].x = 2.5;
+    waypoint_candid[2].y = -8.5;
+    waypoint_candid[3].x = -2.5;
+    waypoint_candid[3].y = -8.0;
+    waypoint_candid[4].x = -3.5;
+    waypoint_candid[4].y = 8.5;
+
+
+    // Waypoints for arbitrary goal points.
+    // TA will change this part before scoring.
+    // This is an example.
+    waypoint_candid[5].x = 1.5;
+    waypoint_candid[5].y = 1.5;
+    waypoint_candid[6].x = -2;
+    waypoint_candid[6].y = -9.0;
+
+    int order[] = {0,1,2,3,4,5,6};
+    int order_size = 7;
+
+    for(int i = 0; i < order_size; i++){
+        waypoints.push_back(waypoint_candid[order[i]]);
+    }
+}
+
+void generate_path_RRT()
+{   
+    //TODO 1
+}
